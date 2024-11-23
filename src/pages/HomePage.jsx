5ce7552061a67f20ff9efd12c47d79f4f5c57345
asaralegi <123814230+asaralegi@users.noreply.github.com>
2024-11-23 20:05:09 +0100

    Dev/T004 Crear servicio de acceso al API (#4)
    - Crear servicio de acceso al API (ApiService)
    - Modificar las páginas Home y Producto para añadir función de acceso al API
    - Limpieza de hojas de estilo del proyecto de inicio
    - Revisión README

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from '../services/ApiService';
+
 function HomePage() {
+
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
   
+    useEffect(() => {
+      const loadProducts = async () => {
+        try {
+          const data = await fetchProducts();
+          setProducts(data);
+        } catch (err) {
+          setError('Error al obtener la lista de productos');
+        }
+      };
+  
+      loadProducts();
+    }, []);
+  
+    if (error) {
+      return <div>{error}</div>;
+    }
+
     return (
       <div className="container mt-4">
         <h1 className="mb-4">Home Page</h1>
+
+        <div>
+            <ul>
+                {products.map((product) => (
+                <li key={product.id}>{product.name}</li>
+                ))}
+            </ul>
+        </div>
       </div>
     );
   }
